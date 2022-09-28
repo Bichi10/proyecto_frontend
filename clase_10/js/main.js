a1031f566d0dd56271ebc19590f2440e06092939
mauri <mastobiza@gmail.com>
2022-09-27 21:51:52 -0300

    clase 10

---
+++ clase_10/js/main.js
@@ -0,0 +1,48 @@
+/*console.log('hola mundo');
+// comentario
+
+//variable con comilla string texto
+let nombre = "Mauricio";
+let apellido = "Astobiza Aguero";
+// sin comillas numero
+let edad = 45;
+
+let mascota=true;
+
+// constante
+const DOLAR = 150;
+
+// mostrar las variables
+console.log(nombre);
+console.log(apellido);
+console.log(edad);
+console.log(mascota);
+
+console.log(nombre,apellido,edad,mascota);
+
+let numeroa=12;
+let numerob=3;
+
+let resultsuma = numeroa + numerob ;
+console.log(resultsuma);
+
+let resultmult = numeroa * numerob ;
+console.log(resultmult);
+
+let textoa= "hola";
+let textob= "mundo!";
+const BLANCO=" ";
+
+console.log(textoa + BLANCO + textob);*/
+
+/*let nombre = prompt(" ingrese su nombre ");
+console.log(nombre);
+const BLANCO=" ";
+let salida = "hola" + BLANCO + nombre;
+alert(salida); */
+
+// llevar un ingreso de usuario a numero para usarlos para sumarlo
+let numero = parseInt(prompt("ingrese un numero: "));
+const numerob =30;
+let salida=numero + numerob;
+alert(salida);
